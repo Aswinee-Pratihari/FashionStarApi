166a32aa0c8379d7e3107f0ad044a8a99dc5dda5
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-08-02 21:13:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+const app=express()
+import "dotenv/config"
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const connectDb=async()=>{
+try {
+    await mongoose.connect(process.env.MONGO_URI)
+
+    console.log("connected to mongodb")
+} catch (error) {
+    console.error('MongoDB connection error:', error);
+}
+}
+
+app.listen(port, () => {
+  connectDb()
+})
